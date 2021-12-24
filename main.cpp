5a26b32293aff37d64fd56d3ac3ce2c924db0196
Aran Kieskamp <aranna00@gmail.com>
2021-12-24 10:24:45 +0100

    switch from alt_alarm to timer

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include "alt_types.h"
 #include "system.h"
 #include "sys/alt_alarm.h"
+#include "altera_avalon_timer.h"
+#include "sys/alt_irq.h"
 
 #include "library/vga/vga.h"
 #include "src/game.h"
@@ -15,12 +17,16 @@
 /**
  * Switch om makkelijk te schakelen tussen een PS2 keyboard en onboard buttons
  **/
-#define USE_INPUT_PS2 // comment deze regel uit als je de onboard buttons wilt gebruiken
+//#define USE_INPUT_PS2 // comment deze regel uit als je de onboard buttons wilt gebruiken
 
 #ifndef USE_INPUT_PS2
+
 #include "library/ial/ial_de2_115.h"
+
 #else
+
 #include "library/ial/ial_ps2.h"
+
 #endif
 
 /**
@@ -35,32 +41,32 @@
  *
  * @param second 	Een heel aantal seconden
  **/
-#define SECONDS_TO_US(second)	(second * 1000000)
+#define SECONDS_TO_US(second)    (second * 1000000)
 /**
  * Macro voor het converteren van ticks per seconden naar delaytime in microseconden
  *
  * @param tps		Het aantal ticks per seconden
  **/
-#define TICKS_TO_US(tps)		((int)(((float)(1.0f/((float)tps)))*SECONDS_TO_US(1)))
+#define TICKS_TO_US(tps)        ((int)(((float)(1.0f/((float)tps)))*SECONDS_TO_US(1)))
 
 /**
  * Macro voor het omzetten van ticks per seconde naar alt_alarm ticks
  *
  * @param tps		Het aantal ticks per seconden
  **/
-#define TPS_TO_TICKS(tps)		(float)((float)((float) alt_ticks_per_second() / (float) tps))
+#define TPS_TO_TICKS(tps)        (float)((float)((float) alt_ticks_per_second() / (float) tps))
 
 /**
  * Hoe vaak onze game per seconde updatet
  **/
-#define TPS						8 // updates per seconde
+#define TPS                        8 // updates per seconde
 
 /**
  * Hoeveelheid alt_alarm ticks
  **/
-#define GAME_TICKS				TPS_TO_TICKS(TPS)
+#define GAME_TICKS                TPS_TO_TICKS(TPS)
 
-#define LOG_INIT_MSG(component)	(LOG_INFO(component " is geinitialiseerd"))
+#define LOG_INIT_MSG(component)    (LOG_INFO(component " is geinitialiseerd"))
 
 /**
  * Het display waar we naar toe renderen.
@@ -80,17 +86,15 @@ static alt_alarm game_tick;
 // In de documentation van alt_alarm staat dat de callback methode
 // in interrupt context wordt uitgevoerd, maar for some reason
 // deze wordt gewoon in user context uitgevoerd... prima voor ons
-alt_u32 game_tick_cb(void *) {
-	// Check voor nieuwe input
-	input->ial_poll();
+void game_tick_cb(void *, alt_u32 ignored) {
+    // Check voor nieuwe input
+    input->ial_poll();
 
-	// If game is running
-	if (game->running) {
-		game->tick(); // refresh game logic
-	}
-	game->render(); // render to screen
-
-	return GAME_TICKS;
+    // If game is running
+    if (game->running) {
+        game->tick(); // refresh game logic
+    }
+    game->render(); // render to screen
 }
 
 /**
@@ -115,20 +119,42 @@ void init() {
     LOG_INIT_MSG("Game");
 }
 
+void start_timer_irq() {
+    alt_avalon_timer_sc_init((void *) INTERVAL_TIMER_BASE,
+                             INTERVAL_TIMER_IRQ_INTERRUPT_CONTROLLER_ID,
+                             INTERVAL_TIMER_IRQ,
+                             INTERVAL_TIMER_FREQ);
+
+    // zie pagina 47 van de DE2-115 computer manual !!!!!!!!
+    // daar gebruiken ze niet alt_irq_* maar doen ze alles handmatig.
+    // nogsteeds handig om ff te lezen wat daar staat
+
+    // we gebruiken geen context maar ok.
+    static unsigned int __isr_context;
+    void *isr_context_ptr = (void *) &__isr_context;
+
+    // registreer de interrupt service handler
+    alt_irq_register(INTERVAL_TIMER_IRQ, isr_context_ptr, game_tick_cb);
+
+    // enable de interrupt
+    alt_irq_enable(INTERVAL_TIMER_IRQ);
+}
 
 /**
  * Het entry point van ons programma
  **/
 int main() {
-	LOG_INFO("Flappy Bird wordt gestart...");
+    LOG_INFO("Flappy Bird wordt gestart...");
 
-	// Initialiseer de randapparaten en de game
+    // Initialiseer de randapparaten en de game
     init();
 
-    // Start de gametick timer
-    if (alt_alarm_start(&game_tick, GAME_TICKS, game_tick_cb, NULL) < 0) {
-    	LOG_ERROR("Fout met initialiseren van systeemklok");
-    }
+
+    start_timer_irq();
+//    // Start de gametick timer
+//    if (alt_alarm_start(&game_tick, GAME_TICKS, game_tick_cb, NULL) < 0) {
+//    	LOG_ERROR("Fout met initialiseren van systeemklok");
+//    }
 
     LOG_INFO("Game tick is gestart");
 
@@ -136,3 +162,4 @@ int main() {
     while (1);
     return 0;
 }
+
